fc91a29f92a72060ba534c52bdbad5d81451278d
Randi Schreiner <mimirandi21@gmail.com>
2020-10-06 15:28:59 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_group)
+        anagram_array = []
+        word_group.each do |subject| 
+            if subject.split('').sort == word.split('').sort
+                anagram_array << subject
+            # else
+            #     anagram_array = []
+            end
+            
+        end
+        return anagram_array
+    end
+end
